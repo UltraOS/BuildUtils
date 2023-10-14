5c3a0df3ca9768a614fa36bf2764a90bf79ea6bd
Daniil Tatianin <99danilt@gmail.com>
2023-10-14 23:25:34 +0300

    Fix all mypy & flake8 warnings
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- package_manager.py
+++ package_manager.py
@@ -1,16 +1,18 @@
 from abc import abstractmethod, ABC
 import platform
 import subprocess
+from typing import Optional
 
 
-def _command_exists(cmd):
+def _command_exists(cmd: str) -> bool:
     ret = subprocess.run(["command", "-v", cmd], stdout=subprocess.DEVNULL,
                          stderr=subprocess.DEVNULL, shell=True)
     return ret.returncode == 0
 
 
 class PackageManager(ABC):
-    cached = None
+    name = "Base"
+    cached: Optional[type['PackageManager']] = None
 
     @staticmethod
     @abstractmethod
@@ -19,11 +21,12 @@ class PackageManager(ABC):
 
     @staticmethod
     @abstractmethod
-    def is_dep_installed(dep) -> bool:
+    def is_dep_installed(dep: str) -> bool:
         pass
 
+    @staticmethod
     @abstractmethod
-    def install_dep(dep):
+    def install_dep(dep: str) -> None:
         pass
 
 
@@ -36,7 +39,7 @@ class Apt(PackageManager):
         return _command_exists(Apt.name)
 
     @staticmethod
-    def is_dep_installed(dep) -> bool:
+    def is_dep_installed(dep: str) -> bool:
         args = ["apt", "--installed", "list", dep, "-qq"]
         out = subprocess.check_output(args, stderr=subprocess.DEVNULL,
                                       text=True)
@@ -45,7 +48,7 @@ class Apt(PackageManager):
         return "[installed" in out
 
     @staticmethod
-    def install_dep(dep):
+    def install_dep(dep: str) -> None:
         if not Apt.did_update:
             subprocess.run(["sudo", "apt-get", "update"])
             Apt.did_update = True
@@ -61,14 +64,14 @@ class Pacman(PackageManager):
         return _command_exists(Pacman.name)
 
     @staticmethod
-    def is_dep_installed(dep) -> bool:
+    def is_dep_installed(dep: str) -> bool:
         ret = subprocess.run(["pacman", "-Qs", dep],
                              stdout=subprocess.DEVNULL,
                              stderr=subprocess.DEVNULL)
         return ret.returncode == 0
 
     @staticmethod
-    def install_dep(dep):
+    def install_dep(dep: str) -> None:
         subprocess.run(["sudo", "pacman", "-Sy", dep, "--noconfirm"],
                        check=True)
 
@@ -81,14 +84,14 @@ class Brew(PackageManager):
         return _command_exists(Brew.name)
 
     @staticmethod
-    def is_dep_installed(dep):
+    def is_dep_installed(dep: str) -> bool:
         ret = subprocess.run(["brew", "list", dep],
                              stdout=subprocess.DEVNULL,
                              stderr=subprocess.DEVNULL)
         return ret.returncode == 0
 
     @staticmethod
-    def install_dep(dep):
+    def install_dep(dep: str) -> None:
         subprocess.run(["brew", "install", dep], check=True)
 
     @staticmethod
@@ -104,7 +107,7 @@ PACKAGE_MANAGERS = (
 )
 
 
-def get_package_manager() -> PackageManager:
+def get_package_manager() -> type[PackageManager]:
     if PackageManager.cached is not None:
         return PackageManager.cached
 
@@ -126,7 +129,7 @@ def get_package_manager() -> PackageManager:
     return PackageManager.cached
 
 
-def install_dependencies(deps):
+def install_dependencies(deps: dict) -> None:
     pm = get_package_manager()
     pm_deps = deps[pm.name]
 
